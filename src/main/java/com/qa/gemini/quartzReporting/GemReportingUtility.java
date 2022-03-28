94d0d20c05d45b4f366a47e697ef58a99cfc4d56
akash garg <MY_NAME@example.com>
2022-03-28 13:32:31 +0530

    changes to utility

--- src/main/java/com/qa/gemini/quartzReporting/GemReportingUtility.java
+++ src/main/java/com/qa/gemini/quartzReporting/GemReportingUtility.java
@@ -15,8 +15,8 @@ public class GemReportingUtility {
 	public static void createReport(String suiteDetail, String stepJson) throws IOException {
 //		String htmlTemplate = FileUtils.readFileToString(new File(ClassLoader.getSystemResource("QuanticReport.html").getFile()), Charset.defaultCharset());
 		String htmlTemplate = IOUtils.toString(ClassLoader.getSystemResourceAsStream("QuanticReport.html"), Charset.defaultCharset());
-		htmlTemplate = htmlTemplate.replace("var obj = '';","var obj = '"+suiteDetail+"';");
-		htmlTemplate = htmlTemplate.replace("var stepobj = '';", "var stepobj = '"+stepJson+"';");
+		htmlTemplate = htmlTemplate.replace("var obj = '';","var obj = "+suiteDetail+";");
+		htmlTemplate = htmlTemplate.replace("var stepobj = '';", "var stepobj = "+stepJson+";");
 		FileUtils.writeStringToFile(new File("GemEcoTestReport.html"), htmlTemplate, Charset.defaultCharset());	
 	}
 	
